21740f6e93ee664c5d07fd5617959a5f7cdb5134
Eddy-ywf <eddyenclave@gmail.com>
2020-11-24 10:29:50 +0700

    remove fb type

--- src/FirebaseConfig/index.tsx
+++ src/FirebaseConfig/index.tsx
@@ -1,15 +1,4 @@
-interface firebaseConfigModel {
-  apiKey: string;
-  authDomain: string;
-  databaseURL: string;
-  projectId: string;
-  storageBucket: string;
-  messagingSenderId: string;
-  appId: string;
-  measurementId: string;
-}
-
-const firebaseConfig: firebaseConfigModel = {
+const firebaseConfig = {
   apiKey: "AIzaSyCy29P1-jGkWluvY6Eb4fk5sb4zIk3jCW0",
   authDomain: "timeleft-7fa83.firebaseapp.com",
   databaseURL: "https://timeleft-7fa83.firebaseio.com",
