1bb3e8476fc1b75da2aca15bf3ef9dc1d281175b
Eddy-ywf <eddyenclave@gmail.com>
2020-12-22 22:07:27 +0700

    update model category

--- src/ResponseModel/ICategoryResponse.ts
+++ src/ResponseModel/ICategoryResponse.ts
@@ -3,6 +3,7 @@ import LanguageResponse from './ILanguageResponse';
 export default interface CategoryResponse {
   uid: string;
   name: LanguageResponse[];
+  image: string;
   length: number;
   creationDate: string;
 }
